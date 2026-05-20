cecad1c6f762ea9610fab7300b2a98fe39805408
Zakaria-info <info.zakariahossain01@gnail.com>
2026-05-20 13:23:18 +0600

    Also added delete or cancel booking api

--- index.js
+++ index.js
@@ -50,6 +50,19 @@ app.get("/bookings", async (req, res) => {
   res.send(result);
 });
 
+// DELETE BOOKING
+app.delete("/bookings/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const query = {
+    _id: new ObjectId(id),
+  };
+
+  const result =
+    await bookingsCollection.deleteOne(query);
+
+  res.send(result);
+});
 
 
 
